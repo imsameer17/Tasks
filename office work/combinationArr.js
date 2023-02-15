206447cf1d950146be1bbc9684d7c3d03d91758f
imsameer17 <74263943+imsameer17@users.noreply.github.com>
2023-02-15 03:35:26 -0800

    Delete office work directory

--- office work/combinationArr.js
+++
@@ -1,8 +0,0 @@
-// Write a program to combine two arrays
-// firstInput = [ 1, 2, 3 ]
-// secondInput = [ 4, 5, 6 ]
-// Output: = [ 1, 2, 3, 4, 5, 6]
-let arr1 = [0,1,2,3,4];
-let arr2 = [5,6,7,8,9];
-let A = [...arr1, ...arr2];
-console.log(A);
